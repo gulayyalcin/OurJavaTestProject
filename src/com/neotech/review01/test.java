e31c9f73e9a280ca6351fd78e31a2b7145036150
Gulay Yalcin <gulayyalcinstudio@gmail.com>
2025-06-25 09:48:11 -0400

    user 1 made changes

--- src/com/neotech/review01/test.java
+++ src/com/neotech/review01/test.java
@@ -15,14 +15,11 @@ public class test {
 	}
 	
 	public void test2() {
-		System.out.println("test");
+		System.out.println("test2");
 
 	}
 
-	public void test3() {
-		System.out.println("test");
-
-	}
+	
 
 
 }
